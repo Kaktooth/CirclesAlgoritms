91ce19039cf509fc91ac986fbe6b046434659335
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2021-03-31 17:12:32 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace WindowsFormsGraphics2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 453);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Runtime: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(865, 453);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Runtime: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(76, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(610, 434);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 53);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Алгоритм Брезенхема";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(897, 515);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 53);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Свій Алгоритм";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Location = new System.Drawing.Point(1247, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 410);
+            this.panel1.TabIndex = 14;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(628, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(610, 434);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1449, 580);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsGraphics2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            using (var g = Graphics.FromImage(pictureBox1.Image))
+            {
+                Brush b = Brushes.Black;
+                float R = (float)Convert.ToDouble(textBox1.Text);
+                float x = 0;
+                float y = R;
+                int X = Convert.ToInt32(textBox2.Text);
+                int Y = Convert.ToInt32(textBox3.Text);
+                float i = 2 * (1 - R);
+                float limit = 0;
+
+              
+                float d = 0;
+                float d2 = 0;
+                while (y >= limit)
+                {
+                   
+                    g.FillRectangle(b, (X + x), (Y + y), 1, 1);
+                    g.FillRectangle(b, (X + x), (Y - y), 1, 1);
+                    g.FillRectangle(b, (X - x), (Y + y), 1, 1);
+                    g.FillRectangle(b, (X - x), (Y - y), 1, 1);
+
+                    if (i < 0) {
+                        d = (2 * i) + (2 * y) - 1;
+                        if (d <= 0)
+                        {
+                            x = x + 1;
+                            i = i + 2 * x + 1;
+                            continue;
+                        }
+                        if (d > 0)
+                        {
+                            x = x + 1;
+                            y = y - 1;
+                            i = i + 2 * x - 2 * y + 2;
+                            continue;
+                        }
+                    }
+                    if (i > 0)
+                    {
+                        d2 = (2 * i) + (2 * x) - 1;
+                        if (d2 <= 0)
+                        {
+                            x = x + 1;
+                            y = y - 1;
+                            i = i + (2 * x) - (2 * y) + 2;
+                            continue;
+                        }
+                        if (d2 > 0)
+                        {
+                            y = y - 1;
+                            i = i - (2 * y) - 1;
+                            continue;
+                        }
+                    }
+                    if (i == 0)
+                    {
+                        x = x + 1;
+                        y = y - 1;
+                        i = i + (2 * x) - (2 * y) + 2;
+                        continue;
+                    }
+                }
+               
+                pictureBox1.Refresh();
+            }
+           
+
+            stopwatch.Stop();
+            label4.Text = stopwatch.Elapsed.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            using (var g = Graphics.FromImage(pictureBox2.Image))
+            {
+                Brush b = Brushes.Black;
+                float R = (float)Convert.ToDouble(textBox1.Text);
+                float x = 0;
+                float y = R;
+                int X = Convert.ToInt32(textBox2.Text);
+                int Y = Convert.ToInt32(textBox3.Text);
+                float d = 2 * (1 - R);
+
+                double E = 0;
+
+                while (y >= 0)
+                {
+                    g.FillRectangle(b, (X + x), (Y + y), 1, 1);
+                    g.FillRectangle(b, (X + x), (Y - y), 1, 1);
+                    g.FillRectangle(b, (X - x), (Y + y), 1, 1);
+                    g.FillRectangle(b, (X - x), (Y - y), 1, 1);
+                    E = 2 * (d + y) - 1;
+                    
+                    if (d < 0 && (int)E <= 0)
+                    {
+
+                        d += 2 * ++x + 1;
+                        continue;
+                    }
+                    if (d > 0 && (int)E > 0)
+                    {
+
+                        d -= 2 * --y + 1;
+                        continue;
+                    }
+                    d += 2 * (++x - --y);
+
+                }
+                pictureBox2.Refresh();
+            }
+
+            stopwatch.Stop();
+            label5.Text = stopwatch.Elapsed.ToString();
+        }
+    }
+}
